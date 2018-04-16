33bc26dfeda48adf0377bad1f84d7f64fcf8ad98
tcorvin <tyson.corvin@gmail.com>
2018-04-16 18:02:18 -0400

    Add project files.

---
+++ Synuit.Toolkit/Types/Platform/DI/IContainer.cs
@@ -0,0 +1,30 @@
+﻿//
+//  Synuit.Toolkit - Application Architecture Tools - Patterns, Types and Components 
+//  Copyright © 2012-2018 The Dude.  All Rights Reserved.
+//
+namespace Synuit.Toolkit.Types.Platform.DI
+{
+   /*
+   Dependency injection container Interface that is responsible for resolving
+   all of the requested dependencies at run time.
+   */
+
+   public interface IContainer
+   {
+      bool ForwardContainerOnCreate { get; set; }
+      
+      //$!!$
+      //bool BootstrapLinked { get; }
+      //void LinkBootstrap(IBootstrap bootStrap);
+      //$
+
+      // --> Register a type and the class that should be resolved to for instantiation
+      void RegisterType<TFrom, TTo>() where TTo : TFrom;
+
+      // --> Register a type and the factory that will handle class resolution and instantiation.
+      //$!!$ void RegisterType<TFrom>(IObjectFactory of, IContext context) where TFrom : class;
+
+      // --> abstract IoC container's resolve
+      T Resolve<T>();
+   }
+}
