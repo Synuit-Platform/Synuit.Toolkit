d3a5fa2c3b30d4742e7a0325912ea580c98eaa9a
tcorvin <tyson.corvin@gmail.com>
2018-04-16 22:20:39 -0400

    Revert "Revert "Add project files.""
    This reverts commit bad64ac8c3d9e4a091d7da8a962e94b9e6e4c99d.

---
+++ Synuit.Toolkit/Platform/DI/ContainerContext.cs
@@ -0,0 +1,20 @@
+﻿//
+//  Synuit.Toolkit - Application Architecture Tools - Patterns, Types and Components 
+//  Copyright © 2012-2018 The Dude.  All Rights Reserved.
+//
+using Synuit.Toolkit.Types.Platform.DI;
+//
+namespace Synuit.Toolkit.Platform.DI
+﻿{
+   public class ContainerContext : IContainerContext
+   {
+      protected IContainer _container;
+
+      public void SetContainer(IContainer container)
+      {
+         this._container = container;
+      }
+
+      public IContainer Container { get { return this._container; } }
+   }
+}
