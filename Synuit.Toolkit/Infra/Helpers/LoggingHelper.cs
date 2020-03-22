877b2d55333dcb77d7dfe4516c327d9a28e898d0
teebone <1872738+tcorvin@users.noreply.github.com>
2020-03-22 13:19:26 -0400

    more refactor

---
+++ Synuit.Toolkit/Infra/Helpers/LoggingHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace Synuit.Toolkit.Infra.Helpers
+{
+   public class LoggingHelper
+   {
+      public static string GetMethodName(MethodBase method)
+      {
+         return $"{method.DeclaringType.FullName.Split('+')[0]}.{method.DeclaringType.FullName.Split('+')[1].Split('<', '>')[1]}";
+      }
+   }
+}
