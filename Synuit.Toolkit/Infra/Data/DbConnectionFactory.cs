95620a08ada3e95718376047cf86948fd77a6970
teebone <1872738+tcorvin@users.noreply.github.com>
2020-03-25 12:30:47 -0400

    changes

---
+++ Synuit.Toolkit/Infra/Data/DbConnectionFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace Synuit.Toolkit.Infra.Data
+{
+   public abstract class DbConnectionFactory 
+   {
+      private readonly string _connection;
+
+      protected DbConnectionFactory(string connection)
+      {
+         _connection = connection;
+      }
+
+      public IDbConnection Create()
+      {
+         var connection = new SqlConnection(_connection);
+         connection.Open();
+
+         return connection;
+      }
+   }
+}
---
+++ Synuit.Toolkit/Infra/Data/DbConnectionFactoryExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Synuit.Toolkit.Infra.Composition;
+using Synuit.Toolkit.Infra.Composition.Types;
+using System;
+
+namespace Synuit.Toolkit.Infra.Data
+{
+   public static class DbConnectionFactoryExtensions
+   {
+      /// <summary>
+      /// Configures the resolution of <typeparamref name="TDbConnectionFactory"/>'s factory.
+      /// </summary>
+      /// <typeparam name="TDbConnection">The DbConnection.</typeparam>
+      /// <param name="services"></param>
+      /// <param name="connection">Database connection string.</param>
+      public static IServiceCollection AddDbConnectionFactory<TDbConnectionFactory>
+      (
+         this IServiceCollection services,
+         string connection
+      )
+      where TDbConnectionFactory : DbConnectionFactory
+      {
+         var parms = new object[] { connection };
+         //
+         Func<TDbConnectionFactory> func(IServiceProvider ctx) =>
+            () => (TDbConnectionFactory)Activator.CreateInstance(typeof(TDbConnectionFactory), parms);
+
+         services.AddSingleton(func);
+
+         return services.AddSingleton<IFactory<TDbConnectionFactory>, Factory<TDbConnectionFactory>>();
+      }
+   }
+}
